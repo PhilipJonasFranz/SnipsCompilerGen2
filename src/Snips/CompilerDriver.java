7049aad0c3020b8a2c8de5be3f3a83b71f83750b
Jonas Franz <47474345+JonasFranz@users.noreply.github.com>
2020-05-19 18:32:58 +0200

    Update comment distance, update test results

--- src/Snips/CompilerDriver.java
+++ src/Snips/CompilerDriver.java
@@ -74,7 +74,7 @@ public class CompilerDriver {
 	public static boolean printErrors = false;
 	
 	public static String printDepth = "    ";
-	public static int commentDistance = 30;
+	public static int commentDistance = 45;
 	
 	public static List<Double> compressions = new ArrayList();
 	
