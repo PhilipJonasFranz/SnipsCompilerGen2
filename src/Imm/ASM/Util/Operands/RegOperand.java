46281d90363360233cfdd4db359019b82dfe1756
Jonas Franz <47474345+JonasFranz@users.noreply.github.com>
2020-07-02 14:34:49 +0200

    Add graphics.sn library, update release

--- src/Imm/ASM/Util/Operands/RegOperand.java
+++ src/Imm/ASM/Util/Operands/RegOperand.java
@@ -3,7 +3,10 @@ package Imm.ASM.Util.Operands;
 public class RegOperand extends Operand {
 
 	public enum REGISTER {
-		R0, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, FP, R12, SP, LR, PC;
+		R0, R1, R2, R3, 
+		R4, R5, R6, R7, 
+		R8, R9, R10, FP, 
+		R12, SP, LR, PC;
 	}
 	
 	public REGISTER reg;
